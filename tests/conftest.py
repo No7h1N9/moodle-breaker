95b431325999578a022d7ae911e911cae91a1539
alekseik1 <1alekseik1@gmail.com>
2020-05-05 13:29:34 +0300

    ref: better fixture structure

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from unittest.mock import Mock
 import os
 import re
+import json
 from pathlib import Path
 from moodle_api.auth import LoginManager
 from moodle_api.problem import TaskMetadata
@@ -24,14 +25,14 @@ def authed_login_manager(login_manager: LoginManager):
     yield login_manager
 
 
-@pytest.fixture(params=basedir.rglob('attempt_page.htm'))
+@pytest.fixture(params=Path('./fixtures/attempt_page/').iterdir())
 def task_metadata_fixture(request):
-    with open(request.param) as f:
-        mock = Mock()
-        contents = f.read()
-        mock.get().content = contents
-        real_cmid = re.findall(r'cmid=\d+', contents)[0][len('cmid='):]
-        real_sesskey = re.findall(r'sesskey=\w+', contents)[0][len('sesskey='):]
-        yield TaskMetadata(
-            mock, 'http://moodle.phystech.edu/mod/quiz/view.php?id=43122'
-        ), real_cmid, real_sesskey
+    with open(request.param / 'index.htm') as page:
+        with open(request.param / 'params.json') as params:
+            mock = Mock()
+            contents = page.read()
+            mock.get().content = contents
+            expected = json.loads(params.read())
+            yield TaskMetadata(
+                mock, 'http://moodle.phystech.edu/mod/quiz/view.php?id=43122'
+            ), expected
--- tests/func/test_parsing.py
+++ tests/func/test_parsing.py
@@ -1,4 +1,4 @@
 def test_get_task_metadata(task_metadata_fixture):
-    task_metadata, cmid, sesskey = task_metadata_fixture
-    assert task_metadata.cmid == cmid
-    assert task_metadata.sesskey == sesskey
+    task_metadata, expected = task_metadata_fixture
+    assert task_metadata.cmid == expected['cmid']
+    assert task_metadata.sesskey == expected['sesskey']
