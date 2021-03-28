f5c35c9a4928b367ae6f43a9f59b110f20ce8343
alekseik1 <1alekseik1@gmail.com>
2021-03-29 00:54:23 +0300

    feat(moodle-api): parser for all courses page

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from pytest_cases import fixture
 basedir = Path(__file__).parent
 
 FIXTURE_PATH = Path(basedir/'fixtures')
+ALL_COURSES_PAGE_DIR = FIXTURE_PATH/'all_courses_page'
 ATTEMPT_PAGE_DIR = FIXTURE_PATH/'attempt_page'
 RUNNING_ATTEMPT_DIR = FIXTURE_PATH/'running_task_page'
 SINGLE_TYPE_DIR = FIXTURE_PATH/'finished_task_page'/'single_type'
@@ -48,4 +49,10 @@ def attempt_page_directory(directory: Path):
 @fixture
 @pytest.mark.parametrize('directory', RUNNING_ATTEMPT_DIR.iterdir(), scope='session', ids=lambda param: param.name)
 def running_attempt_directory(directory: Path):
+    return directory
+
+
+@fixture
+@pytest.mark.parametrize('directory', ALL_COURSES_PAGE_DIR.iterdir(), scope='session', ids=lambda param: param.name)
+def all_courses_page_directory(directory: Path):
     return directory
