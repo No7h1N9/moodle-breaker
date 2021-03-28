8835ecabdf5e007c35d338da07c8cc3252a1b7d6
alekseik1 <1alekseik1@gmail.com>
2021-03-29 00:19:15 +0300

    feat(moodle-api): getter for page with all courses

--- tests/moodle_dependant/cases_basic_api.py
+++ tests/moodle_dependant/cases_basic_api.py
@@ -2,6 +2,6 @@ from dotenv import load_dotenv
 import os
 
 
-def correct_env_login_password():
+def correct_auth_env_login_password():
     load_dotenv()
     return os.environ.get('MOODLE_TEST_LOGIN'), os.environ.get('MOODLE_TEST_PASSWORD')
--- tests/moodle_dependant/test_basic_api.py
+++ tests/moodle_dependant/test_basic_api.py
@@ -1,9 +1,17 @@
-from pytest_cases import parametrize_with_cases
+from pytest_cases import parametrize_with_cases, fixture
 from src.moodle_api.network import MoodleAPI
 
 
-@parametrize_with_cases('login, password', prefix='correct_')
-def test_correct_auth_status_after_login(login, password):
-    api = MoodleAPI(login, password)
-    api.auth()
-    assert api.is_authorized
+@fixture
+@parametrize_with_cases('login, password', prefix='correct_auth_')
+def moodle_api(login, password) -> MoodleAPI:
+    yield MoodleAPI(login, password)
+
+
+def test_correct_auth_status_after_login(moodle_api: MoodleAPI):
+    moodle_api.auth()
+    assert moodle_api.is_authorized
+
+
+def test_get_my_courses_returns_correct_page(moodle_api: MoodleAPI):
+    assert 'Курсы, на которых я учусь' in moodle_api.get_my_courses().text
